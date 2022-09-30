f217428b013c6293a48b7f91a90fbfcd98b13664
McKotter <93869121+mckotter@users.noreply.github.com>
2022-09-30 13:01:57 +0300

    Create harjutus01.java

---
+++ harjutus01.java
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>harjutus01</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>veebilõust.ee</h1>
+        </div>
+        <div class="menu">
+            <ul>
+                <li> <a href="#">xnxx.com</a></li>
+                <li> <a href="#">xvideos.com</a></li>
+                <li> <a href="#">xhamsters.com</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>Lõigud</h1>
+            <p>Lorem ipsum dolor <a href="#">sit amet</a>, consectetur adipisicing elit. Enim natus labore, similique nulla nam molestiae, doloribus tempore pariatur explicabo repudiandae ex at magni cumque dicta recusandae excepturi corrupti obcaecati laudantium!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quae dolores consequatur recusandae quas. Suscipit possimus architecto, molestiae quod at cupiditate nam aut soluta dolor dolorum tenetur. Similique, exercitationem commodi.</p>
+            <hr>
+            <h1>Lisa pilt</h1>
+            <img src="http://placeimg.com/704/308/any" alt="beib">
+            <h2>Täpploend</h2>
+            <ul>
+                <li>Quidem sed nihil in atque</li>
+                <li>Quidem sed nihil in atque</li>
+                <li>Quidem sed nihil in atque</li>
+                <li>Quidem sed nihil in atque</li>
+                <li>Quidem sed nihil in atque</li>
+            </ul>
+        </div>
+        <div class="footer">
+            <p>&copy; Trump 2023</p>
+        </div>
+    </div>
+</body>
+</html>
